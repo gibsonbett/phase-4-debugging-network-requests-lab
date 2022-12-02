4927483be3090abec4ac474487ecc4eb596f6057
Gibson-Bett <gibubett@gmail.com>
2022-12-02 11:12:13 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,15 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
+
   end
 
   def destroy
@@ -23,6 +25,9 @@ class ToysController < ApplicationController
   end
 
   private
+  def render_not_found_response
+    render json: {error: "Toy not found"}, status: :not_found
+  end
   
   def toy_params
     params.permit(:name, :image, :likes)
